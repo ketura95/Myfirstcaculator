2fefbb8f61252d4fd17c0d37b0f114dadcdacf33
t_met <t_met@LAPTOP-0BNJNFOG>
2018-05-24 00:30:08 -0400

    First Commit
    Uploading All the files

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Tracy's Class Caculator");
+		System.out.println(100+2);
+		System.out.println(100*2);
+		System.out.println(100/2);
+		System.out.println(100-2);
+        System.out.println("Goodbye!");
+	}
+
+}
